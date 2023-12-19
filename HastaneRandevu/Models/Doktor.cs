8bb601861c73bdd5ab3eddec9e90a897b3fe4d7d
emirrcaglar <emirrcaglar@gmail.com>
2023-12-19 21:38:50 +0300

    EF install

---
+++ HastaneRandevu/Models/Doktor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HastaneRandevu.Models
+{
+    public class Doktor
+    {
+        [Key] //primary key
+        public int Id { get; set; }
+        [Required] //not null
+        public string Ad { get; set; }
+        [Required]
+        public string Soyad { get; set; }
+        public string Uzmanlik { get; set; }
+        public string Vardiya { get; set; }
+    }
+}
