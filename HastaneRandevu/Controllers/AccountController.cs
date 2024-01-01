e918766a2f415e2fd10e948e3401edb1be6fc27c
Doğancan <Doancan@DESKTOP-F7S6JC4>
2024-01-01 19:31:37 +0300

    login

---
+++ HastaneRandevu/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AccountController : Controller
+{
+    // Diğer aksiyon metotları
+
+    [HttpPost]
+    public ActionResult Login(string username, string password)
+    {
+        // Kullanıcı girişi kontrolü
+        if (IsValidUser(username, password))
+        {
+            // Admin sayfasına yönlendirme
+            return RedirectToAction("Index", "Admin");
+        }
+        else
+        {
+            // Giriş başarısızsa uygun işlemleri yapabilirsiniz
+            return View("Login");
+        }
+    }
+
+    private bool IsValidUser(string username, string password)
+    {
+        // Kullanıcı kimlik doğrulama mantığı buraya eklenir
+        // Örneğin, bir veritabanında kullanıcı bilgilerini kontrol edebilirsiniz
+        // Bu metotun içeriğini projenizin gereksinimlerine göre uyarlayın
+
+        // Örnek bir kontrol
+        return username == "admin" && password == "123456";
+    }
+}
--- HastaneRandevu/Controllers/AdminController.cs
+++ HastaneRandevu/Controllers/AdminController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HastaneRandevu.Controllers
 {
+    
     public class AdminController : Controller
     {
         // GET: HomeController1
